ad213549a2c661c20708cc1dbb419bbe951f948f
jonahgolden <jonahwgolden@gmail.com>
2019-09-01 08:59:27 -0700

    modular project file and small edit to widget modules

--- health-viz-modular/modules/uiModules.R
+++ health-viz-modular/modules/uiModules.R
@@ -1,8 +1,7 @@
 displayButtonInput <- function(id) {
   # Create a namespace function using the provided id
   ns <- NS(id)
-  
-  # The widget
+
   radioGroupButtons(
     ns("display"),
     label = "Display:",
